9cc44c93369dde03c5b7a2b73af9560557f153b4
sin6626 <3554989283@qq.com>
2026-07-27 09:45:45 +0800

    fix: 补充了导航栏左侧的头像

--- app/components/AppHeader.vue
+++ app/components/AppHeader.vue
@@ -67,6 +67,9 @@ onBeforeUnmount(() => window.removeEventListener('resize', updateIndicator))
 </script>
 
 <template>
+  <div class="top-2 sm:top-4 z-10 size-10 fixed">
+    <img src="/Sin.jpg" alt="" class="object-cover rounded-lg">
+  </div>
   <div class="fixed top-2 sm:top-4 left-1/2 transform -translate-x-1/2 z-10">
     <!-- 外层 relative 容器: 为绝对定位的指示条提供定位基准 -->
     <div
