6a09d1e38b6b618d12bae469a60273eb3f215230
sin6626 <3554989283@qq.com>
2026-07-27 16:51:27 +0800

    chore: 清理无依赖的文件,修改部分文字信息

--- content.config.ts
+++ content.config.ts
@@ -2,17 +2,6 @@ import { defineCollection, defineContentConfig, z } from '@nuxt/content'
 
 export default defineContentConfig({
   collections: {
-    images: defineCollection({
-      type: 'page',
-      source: 'images.yml',
-      schema: z.object({
-        images: z.array(z.object({
-          src: z.string(),
-          alt: z.string()
-        }))
-      })
-    }),
-
     index: defineCollection({
       type: 'page',
       source: 'index.yml',
