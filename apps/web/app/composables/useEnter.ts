c0b36b1991e9d4e7aa99ae055986b64a26026175
sin6626 <3554989283@qq.com>
2026-09-25 17:06:49 +0800

    docs: shouping页面动画新增注释

--- apps/web/app/composables/useEnter.ts
+++ apps/web/app/composables/useEnter.ts
@@ -10,8 +10,11 @@ export function useEnter() {
   async function enter(path: string) {
     if (isEntering.value) return
     const to = localePath(path)
-    const sourcePage = pageRef.value
+    const sourcePage = pageRef.value // pageRef 在shouping页面最外加加了
+
+    // CSS的媒体查询当前设备是否关闭了减少动画的选项，GSAP的官方特意说过这个问题
     if (window.matchMedia('(prefers-reduced-motion: reduce)').matches || !sourcePage) {
+      // 如果关闭了动画或者是外层使用这个方法没有记录页面，那么直接跳转
       await navigateTo(to)
       return
     }
@@ -55,8 +58,10 @@ export function useEnter() {
     try {
       // navigateTo 返回时目标页可能仍在加载，尤其是开发环境首次访问。
       await navigateTo(to)
+      // 查看当前的sourcePage是否在挂在在页面上，IsConnected是true就是还在，跳转卡住了，false则可表示页面已经跳转
       if (sourcePage.isConnected) {
         await new Promise<void>((resolve, reject) => {
+          // MutationObserver 是浏览器原生的监听器，监听DOM，只要触发了监听就会触发下面这个检测的逻辑
           const observer = new MutationObserver(() => {
             if (!sourcePage.isConnected) {
               clearTimeout(timeout)
@@ -68,6 +73,7 @@ export function useEnter() {
             observer.disconnect()
             reject(new Error('目标页面渲染超时'))
           }, 15000)
+          // 开启监听，根节点是id为_nuxt的div元素，childList表示监听，有任何变化都能监听，他下面的所有孩子节点，subtree则是表示监听整个DOM树，配合第一个参数，实现递归监听整个树
           observer.observe(document.getElementById('__nuxt') ?? document.body, { childList: true, subtree: true })
           if (!sourcePage.isConnected) {
             clearTimeout(timeout)
